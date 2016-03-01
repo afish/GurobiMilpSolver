6521494429d4413876444ef9dab1a427cbc8c17a
Adam Furmanek <afurmanek@infusion.com>
2016-03-01 19:50:53 +0100

    Added basic implementation.

---
+++ Implementation/GurobiMilpSolver.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using Gurobi;
+using MilpManager.Abstraction;
+using MilpManager.Implementation;
+
+namespace GurobiMilpManager.Implementation
+{
+    public class GurobiMilpSolver : BaseMilpSolver, IDisposable
+    {
+        public GRBEnv Environment { get; protected set; }
+        public GRBModel Model { get; protected set; }
+        public int ConstraintIndex { get; protected set; }
+        protected GRBVar One { get; set; }
+        private bool _disposed;
+        private bool _hasGoal;
+
+        public GurobiMilpSolver(int integerWidth) : base(integerWidth)
+        {
+            Environment = new GRBEnv();
+            Model = new GRBModel(Environment);
+            AddOne();
+        }
+
+        private void AddOne()
+        {
+            One = Model.AddVar(1, 1, 0, GRB.INTEGER, NewVariableName());
+            Model.Update();
+        }
+
+        public override void SetLessOrEqual(IVariable variable, IVariable bound)
+        {
+            Model.AddConstr((variable.Var() ?? variable.ConstantValue.Value*One) <= (bound.Var() ?? bound.ConstantValue.Value * One), NewConstraintName());
+        }
+
+        public override void SetGreaterOrEqual(IVariable variable, IVariable bound)
+        {
+            Model.AddConstr((variable.Var() ?? variable.ConstantValue.Value * One) >= (bound.Var() ?? bound.ConstantValue.Value * One), NewConstraintName());
+        }
+
+        public override void SetEqual(IVariable variable, IVariable bound)
+        {
+            Model.AddConstr((variable.Var() ?? variable.ConstantValue.Value * One) == (bound.Var() ?? bound.ConstantValue.Value * One), NewConstraintName());
+        }
+
+        public override void SaveModelToFile(string modelPath)
+        {
+            if (!_hasGoal)
+            {
+                AddGoal("__dummy", FromConstant(0).Create());
+                _hasGoal = true;
+            }
+            Model.Write(modelPath);
+        }
+
+        public override void Solve()
+        {
+            Model.Optimize();
+        }
+
+        public override double GetValue(IVariable variable)
+        {
+            return variable.Var().Get(GRB.DoubleAttr.X);
+        }
+
+        public override SolutionStatus GetStatus()
+        {
+            int optimstatus = Model.Get(GRB.IntAttr.Status);
+            switch (optimstatus)
+            {
+                case GRB.Status.INFEASIBLE: return SolutionStatus.Infeasible;
+                case GRB.Status.INF_OR_UNBD: return SolutionStatus.Unbounded;
+                case GRB.Status.OPTIMAL: return SolutionStatus.Optimal;
+                case GRB.Status.UNBOUNDED: return SolutionStatus.Unbounded;
+                default: return SolutionStatus.Unknown;
+            }
+        }
+
+        protected override IVariable InternalCreate(string name, Domain domain)
+        {
+            GRBVar variable;
+            if (domain == Domain.AnyConstantInteger || domain == Domain.AnyInteger)
+            {
+                variable = Model.AddVar(double.MinValue, double.MaxValue, 0, GRB.INTEGER, name);
+            }
+            else if (domain == Domain.PositiveOrZeroConstantInteger || domain == Domain.PositiveOrZeroInteger)
+            {
+                variable = Model.AddVar(0, double.MaxValue, 0, GRB.INTEGER, name);
+            }
+            else if (domain == Domain.BinaryConstantInteger || domain == Domain.BinaryInteger)
+            {
+                variable = Model.AddVar(0, 1, 0, GRB.BINARY, name);
+            }
+            else if (domain == Domain.PositiveOrZeroConstantReal || domain == Domain.PositiveOrZeroReal)
+            {
+                variable = Model.AddVar(0, double.MaxValue, 0, GRB.CONTINUOUS, name);
+            }
+            else
+            {
+                variable = Model.AddVar(double.MinValue, double.MaxValue, 0, GRB.CONTINUOUS, name);
+            }
+
+            Model.Update();
+            return new GurobiVariable
+            {
+                GRBVar = variable
+            };
+        }
+
+        protected override IVariable InternalFromConstant(string name, int value, Domain domain)
+        {
+            return InternalFromConstant(name, (double) value, domain);
+        }
+
+        protected override IVariable InternalFromConstant(string name, double value, Domain domain)
+        {
+            return new GurobiVariable();
+        }
+
+        protected override IVariable InternalSumVariables(IVariable first, IVariable second, Domain domain)
+        {
+            var result = CreateAnonymous(domain);
+            Model.AddConstr((first.Var() ?? first.ConstantValue.Value * One) + (second.Var() ?? second.ConstantValue.Value * One) <= result.Var(), NewConstraintName());
+            Model.AddConstr((first.Var() ?? first.ConstantValue.Value * One) + (second.Var() ?? second.ConstantValue.Value * One) >= result.Var(), NewConstraintName());
+            return result;
+        }
+
+        protected override IVariable InternalNegateVariable(IVariable variable, Domain domain)
+        {
+            var result = CreateAnonymous(domain);
+            Model.AddConstr((variable.Var() ?? variable.ConstantValue.Value * One) <= -result.Var(), NewConstraintName());
+            Model.AddConstr((variable.Var() ?? variable.ConstantValue.Value * One) >= -result.Var(), NewConstraintName());
+            return result;
+        }
+
+        protected override IVariable InternalMultiplyVariableByConstant(IVariable variable, IVariable constant, Domain domain)
+        {
+            var result = CreateAnonymous(domain);
+            Model.AddConstr((variable.Var() ?? variable.ConstantValue.Value * One) * (constant.ConstantValue.Value) <= result.Var(), NewConstraintName());
+            Model.AddConstr((variable.Var() ?? variable.ConstantValue.Value * One) * (constant.ConstantValue.Value) >= result.Var(), NewConstraintName());
+            return result;
+        }
+
+        protected override IVariable InternalDivideVariableByConstant(IVariable variable, IVariable constant, Domain domain)
+        {
+            var result = CreateAnonymous(domain);
+            Model.AddConstr((variable.Var() ?? variable.ConstantValue.Value * One) <= (constant.ConstantValue.Value) * result.Var(), NewConstraintName());
+            Model.AddConstr((variable.Var() ?? variable.ConstantValue.Value * One) >= (constant.ConstantValue.Value) * result.Var(), NewConstraintName());
+            return result;
+        }
+
+        protected override object GetObjectsToSerialize()
+        {
+            return ConstraintIndex;
+        }
+
+        protected override void InternalDeserialize(object o)
+        {
+            ConstraintIndex = (int) o;
+            foreach (var variable in Variables)
+            {
+                var typed = variable.Value as GurobiVariable;
+                typed.GRBVar = Model.GetVarByName(typed.Name);
+            }
+        }
+
+        protected override void InternalLoadModelFromFile(string modelPath)
+        {
+            Model.Dispose();
+            Model = new GRBModel(Environment, modelPath);
+            _hasGoal = true;
+            AddOne();
+        }
+
+        protected override void InternalAddGoal(string name, IVariable operation)
+        {
+            Model.SetObjective(operation.Var() * One, GRB.MAXIMIZE);
+        }
+
+        protected virtual string NewConstraintName()
+        {
+            return $"_c_{ConstraintIndex++}";
+        }
+
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                Model.Dispose();
+                Environment.Dispose();
+            }
+            
+            _disposed = true;
+        }
+    }
+}
---
+++ Implementation/GurobiVariable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Gurobi;
+using MilpManager.Abstraction;
+
+namespace GurobiMilpManager.Implementation
+{
+    [Serializable]
+    public class GurobiVariable : IVariable
+    {
+        [NonSerialized]
+        private GRBVar _grbVar;
+        [NonSerialized]
+        private IMilpManager _milpManager;
+
+        public IMilpManager MilpManager
+        {
+            get { return _milpManager; }
+            set { _milpManager = value; }
+        }
+
+        public Domain Domain { get; set; }
+        public string Name { get; set; }
+        public double? ConstantValue { get; set; }
+        public string Expression { get; set; }
+
+        public GRBVar GRBVar
+        {
+            get { return _grbVar; }
+            set { _grbVar = value; }
+        }
+    }
+
+    public static class GurobiVariableExtensions
+    {
+        public static GurobiVariable Typed(this IVariable variable)
+        {
+            return variable as GurobiVariable;
+        }
+
+        public static GRBVar Var(this IVariable variable)
+        {
+            return variable.Typed().GRBVar;
+        }
+    }
+}
